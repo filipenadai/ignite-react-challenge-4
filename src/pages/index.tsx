ab6f1df5b9c2f07337775920057e2a7698445d0d
filipenadai <f.nadai05@gmail.com>
2021-10-05 22:14:00 -0300

    add home page init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +29,60 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>spacestravelers</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {postsPagination.results.map(post => (
+            <a key={post.uid}>
+              <strong>{post.data.title}</strong>
+              <span>{post.data.subtitle}</span>
+              <div>
+                <FiCalendar />
+                <time>{post.first_publication_date}</time>
+                <FiUser style={{ marginLeft: 24 }} size={20} color="" />
+                <p>{post.data.author}</p>
+              </div>
+            </a>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 100,
+    }
+  );
+
+  const resultsSerialized = postsResponse.results.map(result => ({
+    ...result,
+    first_publication_date: format(
+      new Date(result.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+  }));
+
+  return {
+    props: {
+      postsPagination: {
+        ...postsResponse,
+        results: resultsSerialized,
+      },
+    },
+  };
+};
